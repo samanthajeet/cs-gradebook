3c52859853bdf6533caa8dbdcd11117591791d8e
Sam Jeet <samanthajeet@gmail.com>
2022-04-08 10:42:10 -0600

    Controlling the flow of execution:

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -42,7 +42,7 @@ namespace GradeBook
                 grades.Add(grade);
             }
             else {
-                Console.WriteLine("Invalid value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
         public Statistics GetStatistics()
@@ -60,6 +60,25 @@ namespace GradeBook
             }
 
             result.Average = result.Average / grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
             
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,13 +8,49 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Benson's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            
+
+            var done = false;
+
+            while(!done)
+            {
+
+                Console.WriteLine("Enter a grade or 'q' to quite");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    done = true;
+                    continue;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    // code you always want to execute
+                    Console.WriteLine("**");
+                }
+                
+            }           
+
             var stats = book.GetStatistics();
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The latter grade is {stats.Letter}");
+
         }
     }
 }
